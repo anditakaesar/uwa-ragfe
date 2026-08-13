768efb98e39a2036ce8e801d5a41f1d5c512c8f7
Andita Fahmi <anditakaesar@gmail.com>
2026-08-13 09:25:43 +0700

    fix: imported fonts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,11 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.scss'
 import App from './App.tsx'
+import '@fontsource/ibm-plex-sans/600.css';
+import '@fontsource/ibm-plex-sans/100.css';
+import '@fontsource/ibm-plex-sans/400.css';
+import '@fontsource/ibm-plex-sans/300.css';
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
